99bbb5c58f72a924a73165ea269aee09f97258c4
MinYu <yurimin@foxmail.com>
2016-08-08 11:17:44 +0800

    增加sqlite处理封装,基于xutils

--- LibMyApp/src/main/java/com/xtkj/libmyapp/application/MyApplication.java
+++ LibMyApp/src/main/java/com/xtkj/libmyapp/application/MyApplication.java
@@ -11,21 +11,27 @@ import com.xtkj.libmyapp.util.ServerConfig;
 
 public class MyApplication extends Application {
 
+    public static Application myapp;
     public VersionControl vc;
 
-	@Override
-	public void onCreate() {
-		super.onCreate();
-		//初始化配置文件模块
-		ServerConfig.initServerConfig(getApplicationContext());
-		//初始化文件类
-		FileUtil.init(getApplicationContext());
-		//相册初始化
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        if (myapp == null) {
+            myapp = this;
+        }
+        //初始化配置文件模块
+        ServerConfig.initServerConfig(getApplicationContext());
+        //初始化文件类
+        FileUtil.init(getApplicationContext());
+        //相册初始化
         GalleryHelper.init(getApplicationContext());
-		//初始化日志类
-		MyLog.initFileDir(FileUtil.getAppExtFilesPath());
+        //初始化日志类
+        MyLog.initFileDir(FileUtil.getAppExtFilesPath());
 
         //初始化版本控制
-        vc=new VersionControl(getApplicationContext());
-	}
+        vc = new VersionControl(getApplicationContext());
+    }
+
 }
---
+++ LibMyApp/src/main/java/com/xtkj/libmyapp/db/converter/ColumnConverter.java
@@ -0,0 +1,19 @@
+package com.xtkj.libmyapp.db.converter;
+
+import android.database.Cursor;
+
+import com.xtkj.libmyapp.db.sqlite.ColumnDbType;
+
+/**
+ * Author: wyouflf
+ * Date: 13-11-4
+ * Time: 下午8:57
+ */
+public interface ColumnConverter<T> {
+
+    T getFieldValue(final Cursor cursor, int index);
+
+    Object fieldValue2DbValue(T fieldValue);
+
+    ColumnDbType getColumnDbType();
+}
